67e1b5f4814823db7828e1cb0106e0b4f71d2dbe
Fábio <fabioec.2010@gmail.com>
2020-02-17 13:27:45 -0400

    Firt Commit

---
+++ src/main/java/br/com/jbf/Jato/Boost/Framework/JatoBoostFrameworkApplication.java
@@ -0,0 +1,13 @@
+package br.com.jbf.Jato.Boost.Framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JatoBoostFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JatoBoostFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jbf/Jato/Boost/Framework/JatoBoostFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jbf.Jato.Boost.Framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JatoBoostFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
